1b4d6abe9c7c6f0caee0bba2e826b05efc3895a4
Kelsie Braidwood <kelsieiona@gmail.com>
2018-06-08 22:47:57 +0100

    record constructor created

---
+++ record.js
@@ -0,0 +1,11 @@
+// - Create a Record object that has an Artist, Title, Genre, and Price
+// - Create a Record Store that has a Name, City and an Inventory.
+// - Give the Record Store a Balance.
+// - Add some Records to the Store's Inventory.
+
+var Record = function(artist, title, genre, price){
+  this.artist = artist;
+  this.title = title;
+  this.genre = genre;
+  this.price = price;
+}
