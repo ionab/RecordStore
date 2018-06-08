4636d89df071fac96ea93c472c9a20095115993a
Kelsie Braidwood <kelsieiona@gmail.com>
2018-06-08 23:05:03 +0100

    adds record store constructor and tests

---
+++ specs/record_store_spec.js
@@ -0,0 +1,23 @@
+var assert = require("assert");
+var Record = require("../record.js");
+var RecordStore = require("../record_store.js");
+
+describe("RecordStore", function(){
+  var record;
+  var recordStore;
+
+  beforeEach(function(){
+    record = new Record("Artist1", "Title1", "Rock", 19.99)
+    recordStore = new RecordStore("Kelsie's Record Store", "Glasgow")
+  })
+
+  it("should have a name", function(){
+    assert.strictEqual(recordStore.name, "Kelsie's Record Store")
+  })
+  it("should have a name", function(){
+    assert.strictEqual(recordStore.city, "Glasgow")
+  })
+  it("should have an inventory which starts empty", function(){
+    assert.strictEqual(recordStore.inventory.length, 0)
+  })
+})
