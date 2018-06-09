9d017495ac74f0385845395c812fa58a4fc2ff82
Kelsie Braidwood <kelsieiona@gmail.com>
2018-06-09 20:57:57 +0100

    adds should statements to specs file based on brief:

---
+++ specs/customer_spec.js
@@ -0,0 +1,19 @@
+
+
+
+- Create a RecordCollector (or Customer) constructor
+who can buy and sell records.
+- The RecordCollector should have cash that increase and decreases with buying and selling.
+- The RecordCollector shouldn't be able to buy a Record if he can't afford it.
+- The RecordCollector should be able to view the total value of their collection
+- The RecordCollector should be able to view the total value of all records of a given Genre
+- The RecordCollector should be able to view their most valuable record.
+- The RecordCollector should be able to sort their records by value. (ascending or descending)
+- The RecordCollector should be able to compare the value of their collection with another RecordCollector
+
+should be able to buy records
+should be able to return records
+should have cash which starts at zero
+should increase cash  when record returned
+should decrease cash when record bought
+should be able to view the total value of 
